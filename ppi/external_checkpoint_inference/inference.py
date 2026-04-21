6e83292196901875f920656f270849dd25eefedc
Mateusz Matuszak <mateusz_matuszak@hotmail.com>
2026-04-21 09:46:58 +0000

    simple script for running multiple inferences

--- ppi/external_checkpoint_inference/inference.py
+++ ppi/external_checkpoint_inference/inference.py
@@ -1,9 +1,12 @@
 import os
 import ruamel.yaml
+from datetime import datetime, timedelta
+import numpy as np
 import sys
+import logging
+logger = logging.getLogger(__name__)
 
-
-def set_date_in_yaml(input_yaml, date):
+def set_date(input_yaml, date):
     '''
         Updates the date in yaml file
     Args:
@@ -21,9 +24,48 @@ def set_date_in_yaml(input_yaml, date):
     
     with open(input_yaml, 'w') as f:
         yaml.dump(data, f)
+    
+def inference(input_yaml, start, end):
+    '''
+        Runs inference for multiple dates.
+    Args:
+        input_yaml  [str]   :   name of yaml file   
+        start       [str]   :   start time 
+        end         [str]   :   end time
+    '''
+    logging.basicConfig(level=logging.INFO)
+    start =  datetime.strptime(start, '%Y-%m-%d')
+    end = datetime.strptime(end, '%Y-%m-%d') + timedelta(days=1) #adding extra to include the last day
+    times = np.arange(start, end, timedelta(days=1)).astype(datetime)
+    logger.info(f'Running inference for dates between {start} - {end}.')
+
+    for time in times:
+        date = f'{time.year}-{time.month:02d}-{time.day:02d}'
+        logger.info(f'Starting inference for {date}')
+        set_date(input_yaml, date)
+        
+        os.system(f'anemoi-inference run {input_yaml}')
+        os.system(f'python ../postpro-inference.py {input_yaml}')
 
 
 if __name__ == '__main__':
     import argparse
-    set_date_in_yaml('infer.yaml', '---')
+    parser = argparse.ArgumentParser(
+        prog='long inference'
+    )
+    parser.add_argument(
+        '-f', '--file', type=str, required=True, help='Input yaml file'   
+    )
+    parser.add_argument(
+        '-s', '--start', type=str, required=True, help='Start date, %Y-%m-%d'
+    )
+    parser.add_argument(
+        '-e', '--end', type=str, required=True, help='End date, %Y-%m-%d'
+    )
+    args = parser.parse_args()
 
+    inference(
+        input_yaml=args.file,
+        start=args.start,
+        end=args.end
+    )
