5ad12fcddea6ece031abcf59510983a0b10a1a1e
Mateusz Matuszak <mateusz_matuszak@hotmail.com>
2026-04-21 08:29:39 +0000

    function to update date in yaml

---
+++ ppi/external_checkpoint_inference/inference.py
@@ -0,0 +1,25 @@
+import os
+import ruamel.yaml
+import sys
+
+
+def set_date_in_yaml(input_yaml, date):
+
+
+    yaml = ruamel.yaml.YAML()
+    with open(input_yaml) as f:
+        data = yaml.load(f)
+    
+    if 'date' in data:
+        data['date'] = date
+    else:
+        raise ValueError(f'No key date found in {input_yaml}')
+    
+    with open(input_yaml, 'w') as f:
+        yaml.dump(data, f)
+
+
+if __name__ == '__main__':
+    import argparse
+    set_date_in_yaml('infer.yaml', '---')
+
