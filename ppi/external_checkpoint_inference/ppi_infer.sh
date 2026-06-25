76bb38ff3151b9d74874ea92dd65df3fe9ecc731
Mateusz Matuszak <mateusz_matuszak@hotmail.com>
2026-06-25 08:01:49 +0000

    standard venv, no need to run setup.sh

--- ppi/external_checkpoint_inference/ppi_infer.sh
+++ ppi/external_checkpoint_inference/ppi_infer.sh
@@ -8,7 +8,8 @@
 #SBATCH --mem=80G
 #SBATCH --ntasks-per-node=1
 
-source $(pwd -P)/.venv/bin/activate
+#source $(pwd -P)/.venv/bin/activate
+source /lustre/storeB/project/fou/hi/foccus/python-envs/inference-env-jun26/bin/activate
 
 CONFIG_DIR=$(pwd -P)/
 CONFIG_NAME=$CONFIG_DIR/infer.yaml
